d973ab802a96d79d5b6b848d75ec957ae145e136
Flying-Fairy <axl.peroys@outlook.fr>
2024-12-01 06:38:42 +0100

    first commit

---
+++ python/day1.py
@@ -0,0 +1,28 @@
+# part 1
+total = 0
+ls_one = []
+ls_two = []
+
+with open("inputs/input1.txt") as f:
+    for line in f.readlines():
+        a, b = line.strip().split("   ")
+        
+        ls_one.append(int(a))
+        ls_two.append(int(b))
+        
+ls_one.sort()
+ls_two.sort()
+
+for i in range(len(ls_one)):
+    total += abs(ls_one[i] - ls_two[i])
+
+print("part 1:", total)
+
+
+# part 2
+total = 0
+
+for i in ls_one:
+    total += i * ls_two.count(i)
+
+print("part 2:", total)
